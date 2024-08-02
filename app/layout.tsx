ad0b7e18fd7465acb10d533f77dd1a1d277967c4
kj3002 <kevinji@umich.edu>
2024-08-02 21:47:06 +0900

    Moved around og

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Home',
+    template: '%s | My Website',
   },
   description: 'This is my portfolio.',
   openGraph: {
